9f076bf371cc4cede8c7dfec92f4b33f8ace9d9e
kernel688 <jcmartinezcano@gmail.com>
2022-03-11 18:04:40 -0500

    MultipleTableSorting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MultipleTableSortingComponent } from './pages/multiple-table-sorting/multiple-table-sorting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'multiple-table-sorting',
+    component: MultipleTableSortingComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MultipleTableSortingComponent } from './pages/multiple-table-sorting/multiple-table-sorting.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultipleTableSortingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
